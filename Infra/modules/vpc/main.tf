9ba3958058658cfe95ef57995aca92835313826e
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 00:13:57 +0530

    Update main.tf

--- Infra/modules/vpc/main.tf
+++ Infra/modules/vpc/main.tf
@@ -2,11 +2,20 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "public" {
+resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 }
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
 
 resource "aws_security_group" "allow_http" {
   vpc_id = aws_vpc.main.id
@@ -37,9 +46,10 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_id" {
-  value = aws_subnet.public.id
+output "public_subnet_ids" {
+  value = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 }
+
 
 output "security_group_id" {
   value = aws_security_group.allow_http.id
