e4dd50d688f1b52ec5d2beeeaee760bfb28037c2
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 23:55:57 +0530

    Create variables.tf

---
+++ Infra/modules/alb/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security Group ID"
+  type        = string
+}
+
+variable "instance_id" {
+  description = "EC2 Instance ID"
+  type        = string
+}
