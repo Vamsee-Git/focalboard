9687148aa3770da158f5a0a0043b061b3446db1a
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-15 23:55:07 +0530

    Create main.tf

---
+++ Infra/modules/alb/main.tf
@@ -0,0 +1,43 @@
+resource "aws_lb" "main" {
+  name               = "focalboard-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.security_group_id]
+  subnets            = [var.subnet_id]
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "focalboard-targets"
+  port     = 8000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "main" {
+  target_group_arn = aws_lb_target_group.main.arn
+  target_id        = var.instance_id
+  port             = 8000
+}
+
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
