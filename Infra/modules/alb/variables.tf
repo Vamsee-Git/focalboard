b92ca668f6f4223df4ae0536246b2fe6aa137755
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-05-16 06:45:41 +0530

    Update variables.tf

--- Infra/modules/alb/variables.tf
+++ Infra/modules/alb/variables.tf
@@ -4,9 +4,9 @@ variable "vpc_id" {
 }
 
 variable "subnet_ids" {
-  description = "Subnet ID"
-  type        = string
+  type = list(string)
 }
+
 
 variable "security_group_id" {
   description = "Security Group ID"
